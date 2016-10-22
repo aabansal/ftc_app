0855982b0efe64be123f5cfa994aa2284de5f791
heatblast016 <adiddee@live.com>
2016-10-21 19:53:24 -0700

    Revert "wut"
    This reverts commit e8909cb3fb8dd20aa3c03c4dea9d3eec42860fa3.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TemplateOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TemplateOpMode_Linear.java
@@ -53,6 +53,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Template: Linear OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@Disabled
 public class TemplateOpMode_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
